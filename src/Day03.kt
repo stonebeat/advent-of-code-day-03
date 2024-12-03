f42b2df0c8df1fe4fd9f5b11eb296df3f9fcaaf9
achim.weste <habe@keine.de>
2024-12-03 08:30:12 +0100

    Day03 solution - Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,19 +1,43 @@
 fun main() {
     fun part1(input: String): Int {
-        val regex = Regex("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
-        return regex.findAll(input).map { it.groupValues[1].toInt() * it.groupValues[2].toInt() }.sum()
+        val regex = Regex("mul\\((?<f1>[0-9]{1,3}),(?<f2>[0-9]{1,3})\\)")
+        return regex.findAll(input).map {
+            val f1 = it.groups.get("f1")?.value!!.toInt()
+            val f2 = it.groups.get("f2")?.value!!.toInt()
+            f1 * f2 }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val regex = Regex("((?<do>(do|don't))\\(\\).*?)?mul\\((?<f1>[0-9]{1,3}),(?<f2>[0-9]{1,3})\\)")
+        val matches = regex.findAll(input)
+        var mulIsActivated = true
+        var sum = 0
+
+        for (match in matches) {
+            var instructionDo = match.groups.get("do")?.value
+            mulIsActivated = when (instructionDo) {
+                "do" -> true
+                "don't" -> false
+                else -> mulIsActivated
+            }
+
+            if (!mulIsActivated) continue
+
+            val f1 = match.groups.get("f1")?.value!!.toInt()
+            val f2 = match.groups.get("f2")?.value!!.toInt()
+            sum += f1 * f2
+        }
+        return sum
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day03_test")
-    check(part1(testInput) == 161)
+    val testInputPart1 = readInput("Day03_test_part1")
+    val testInputPart2 = readInput("Day03_test_part2")
+    check(part1(testInputPart1) == 161)
+    check(part2(testInputPart2) == 48)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
